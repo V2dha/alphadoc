9a17a5c7b3b7829fb5a71f81db91b69eb64f0d3b
Vividha <rvividha@gmail.com>
2020-11-22 21:25:27 +0530

    resolved tests issues

--- alphacode/test_learn.py
+++ alphacode/test_learn.py
@@ -4,7 +4,7 @@ from click.testing import CliRunner
 
 def test_main(): 
     runner = CliRunner()
-    result = runner.invoke(main, ['src/comment.py'])
+    result = runner.invoke(main, ['alphacode/comment.py'])
     assert result.exit_code == 0
     assert "Function 1 : get_comments\n-----------------------\nFunction 2 : get_comment_blocks\n-----------------------\nFunction 3 : get_doc_blocks\n-----------------------\n" in result.output
 
