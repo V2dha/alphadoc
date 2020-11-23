839c95fd982c8bb3b8930109f2521547a28699ab
Vividha <rvividha@gmail.com>
2020-11-23 23:46:54 +0530

    update setup.py v 1.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
   
 setup( 
         name ='alphadoc', 
-        version ='1.0.1',  
+        version ='1.0.2',  
         url ='https://github.com/MLH-Fellowship/alphadoc', 
         description ='Automatic style guide and docstrings generator for Python code', 
         long_description = long_description, 
